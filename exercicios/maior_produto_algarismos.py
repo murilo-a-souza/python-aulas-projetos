eb38db1b1c3ee78b69f9b92c17423891ff6867b6
murilo-a-souza <sir.murilos@gmail.com>
2026-08-03 10:44:36 -0300

    Exercicio : Maior produto de dígitos de algarismo de um número indicado.

---
+++ exercicios/maior_produto_algarismos.py
@@ -0,0 +1,24 @@
+def receber_numero(algarismos: string = '0'):
+    algDivs = list(algarismos)
+    return algDivs
+
+def encontrar_maior_produto(algDivs : list):
+    produto = 0
+    for x in range(len(algDivs)):
+        algDivs[x] = int(algDivs[x])
+        for y in range(x+1, len(algDivs)): #percorre a lista na posição 1
+            algDivs[y] = int(algDivs[y])
+            if algDivs[x] * algDivs[y] > produto:
+                produto = algDivs[x] * algDivs[y]
+                resultado = f"O maior produto entre os algarismos do número indicado é entre {algDivs[x]} e {algDivs[y]} cujo resultado é {produto}"
+    return resultado
+
+while True:
+    algarismos = input("Irei encontrar o maior produto entre as multiplicações dos algarismos do número indicado, digite aqui (0 para encerrar):")
+    if algarismos == "0":
+        break
+    print(encontrar_maior_produto(receber_numero(algarismos)))
+    
+
+
+
